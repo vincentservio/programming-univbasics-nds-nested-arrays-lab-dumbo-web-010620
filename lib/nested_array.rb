bd5b61962d8e689164c57da16fc833fd331ca4b9
vincent servio <vincentservio@gmail.com>
2019-12-02 05:36:15 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,6 +1,20 @@
 # Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
 
+ENNET_HOUSE = [
+  "Don Gately",
+  "Joelle van Dyne",
+  "Pat M.",
+  "Kate Gompert",
+  "Bruce Green"
+]
 
+ENFIELD_TENNIS_ACADEMY = [
+  "Hal Incandenza",
+  "Lyle",
+  "Gerhard Schtitt",
+  "Mario Incandenza",
+  "Michael Pemulis"
+]
 
 def assembled_matrix
 matrix = [
